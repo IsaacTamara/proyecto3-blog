c70a2fb47a2655ffe15531a68ef60e6dabebbcc3
Isaac Tamara <isaactamara01@gmail.com>
2023-02-14 10:33:11 -0500

    second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 const express = require('express')
+const postRouter = require('./posts/posts.router')
+const db = require('./utils/database')
+
+//? Initial configs 
 const app = express()
 
+app.use(express.json())
 
+db.authenticate()
+    .then(() => {
+        console.log('Database credentials are correct')
+    })
+    .catch((err) => {
+        console.log(err) 
+    })
+
+db.sync() 
+    .then(() => {
+        console.log('The synchronized database')
+    })
+    .catch(err => {
+        console.log(err) 
+    })
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Server Ok!',
+        routes: {
+            posts: 'http://localhost:9000/api/v1/posts'
+        }
+    })
+})
+
+app.use('/api/v1', postRouter)
+
+app.listen(9000, () => {
+    console.log('Server started at port 9000')
+})
 
 module.exports = app
--- src/posts/posts.controllers.js
+++ src/posts/posts.controllers.js
@@ -1,27 +1,53 @@
+const { clearConfigCache } = require('prettier')
+const Posts = require('../models/posts.models')
 
 const findAllPosts = async() => {
     //? Your code here:
+    const data = await Posts.findAll()
+    return data
+}
+
+const findPostById = async(id) => {
+    //? Your code here:
+    const data = await Posts.findOne({
+        where: {
+            id : id
+        }
+    })
+    return data
+}
+
+const createPost = async(postObj) => {
+    //? Your code here:
+    const newPosts = {
+        content: postObj.content,
+        userName: postObj.userName
+    }
+
+    const data = await Posts.create(newPosts)
+    console.log(data, 'messager')
+    return data
 
 }
 
-const findPostById = async() => {
+const updatePost = async(id, postObj) => {
     //? Your code here:
-
+    const data = await Posts.update(postObj, {
+        where: {
+            id : id
+        }
+    })
+    return data
 }
 
-const createPost = async() => {
+const deletePost = async(id) => {
     //? Your code here:
-
-}
-
-const updatePost = async() => {
-    //? Your code here:
-
-}
-
-const deletePost = async() => {
-    //? Your code here:
-
+    const data = await Posts.destroy({
+        where: {
+            id: id
+        }
+    })
+    return data
 }
 
 module.exports = {
